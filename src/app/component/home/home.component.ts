70898446f3faf6495ccdbf5020055a759f5679de
NenupharePro <d.caye@tech-nature.com>
2023-03-30 13:58:35 +0200

    Remplacement

---
+++ src/app/component/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ManagerSushiBoxService } from 'src/app/service/manager-sushi-box.service';
+import { environment } from 'src/environments/environment';
+import { IBox } from 'src/model/IBox';
+
+@Component({
+  selector: 'app-home',
+  templateUrl:'./home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  sushiBoxes: IBox[] = []
+  pathImage: string = "http://localhost:8080/api/image/"
+
+  constructor(private managerSushiBoxservice: ManagerSushiBoxService) {
+    this.pathImage = environment.pathImage
+  }
+
+  ngOnInit(): void {
+    this.managerSushiBoxservice.getSushiBoxes().subscribe(
+      {
+      next:(boxes: IBox[]) => {
+        this.sushiBoxes = boxes
+        console.log(boxes)
+      }, 
+      error: er => console.log(er)
+    })
+  }
+
+}
